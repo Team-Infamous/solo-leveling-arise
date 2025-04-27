106950cd1ab698c1111872cf497d81d767cbcd13
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-04-27 20:18:22 +0545

    ㅤㅤ

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 // src/pages/_app.js
 
 import '../styles/globals.css' // Correct relative path
+import AuthContext from '../context/AuthContext';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
