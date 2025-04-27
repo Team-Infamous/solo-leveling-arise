d6691d0c986a62678b2f90b7729d3a0ae29172bc
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-04-27 20:14:28 +0545

    ㅤㅤ

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 // src/pages/_app.js
-import '@/styles/globals.css' // Only global CSS import here
+
+import '../styles/globals.css' // Correct relative path
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
