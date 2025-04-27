8cbe549ed8a2ecda05d10cefac47d17b207ee66b
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-04-27 19:35:33 +0545

    ㅤㅤ

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+// src/pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#6b21a8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );    
+}
